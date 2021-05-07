16c2254c5147a839fde718cf4fb3404256c44357
Jeff Klenzing <jklenzing@gmail.com>
2021-05-07 10:30:14 -0400

    STY: modernize setup

--- setup.py
+++ setup.py
@@ -4,49 +4,7 @@
 # Full license can be found in License.md
 # -----------------------------------------------------------------------------
 
-import os
-import codecs
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-here = os.path.abspath(os.path.dirname(__file__))
-with codecs.open(os.path.join(here, 'description.txt'), encoding='utf-8') as f:
-    long_description = f.read()
-version_filename = os.path.join('pysatSeasons', 'version.txt')
-with codecs.open(os.path.join(here, version_filename)) as version_file:
-    version = version_file.read().strip()
-
-# change setup.py for readthedocs - commented for now
-# on_rtd = os.environ.get('READTHEDOCS') == 'True'
-install_requires = ['pysat', 'numpy', 'pandas<0.25', 'matplotlib']
-
-
-# Run setup
-
-setup(name='pysatSeasons',
-      version=version,
-      url='http://github.com/pysat/pysatSeasons',
-      author='Russell Stoneback, Jeff Klenzing',
-      author_email='rstoneba@utdallas.edu',
-      description='Seasonal analysis routines for pysat',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages=find_packages(),
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Topic :: Scientific/Engineering :: Astronomy",
-          "Topic :: Scientific/Engineering :: Physics",
-          "Topic :: Scientific/Engineering :: Atmospheric Science",
-          "Intended Audience :: Science/Research",
-          "License :: OSI Approved :: BSD License",
-          "Natural Language :: English",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-      ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      )
+setup()
