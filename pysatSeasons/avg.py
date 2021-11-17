e7437a711cdc13904a143879c75c1946dc318795
Russell Stoneback <rstoneba@utdallas.edu>
2021-11-17 13:45:22 -0600

    STY: Comments pass

--- pysatSeasons/avg.py
+++ pysatSeasons/avg.py
@@ -6,9 +6,9 @@ Instrument independent seasonal averaging routine. Supports averaging
 1D and 2D data.
 """
 
+import collections
 import numpy as np
 import pandas as pds
-import collections
 import pysat
 import pysatSeasons as ssnl
 
@@ -23,19 +23,19 @@ def median1D(const, bin1, label1, data_label, auto_bin=True, returnData=False):
     binX: array-like
         List holding [min, max, number of bins] or array-like containing
         bin edges, where X = 1, 2
-    labelX: string
+    labelX: str
         identifies data product for binX, where X = 1, 2
     data_label: list-like
         contains strings identifying data product(s) to be averaged
-    auto_bin: boolean
+    auto_bin: bool
         if True, function will create bins from the min, max and
         number of bins. If false, bin edges must be manually entered
-    returnData : boolean
+    returnData : bool
         Return data in output dictionary as well as statistics
 
     Returns
     -------
-    median : dictionary
+    median : dict
         1D median accessed by data_label as a function of label1
         over the season delineated by bounds of passed instrument objects.
         Also includes 'count' and 'avg_abs_dev' as well as the values of
@@ -43,56 +43,53 @@ def median1D(const, bin1, label1, data_label, auto_bin=True, returnData=False):
 
     """
 
-    # const is either an Instrument or a Constellation, and we want to
-    #  iterate over it.
-    # If it's a Constellation, then we can do that as is, but if it's
-    #  an Instrument, we just have to put that Instrument into something
-    #  that will yeild that Instrument, like a list.
+    # `const` is either an Instrument or a Constellation, and we want to
+    #  iterate over it. If it's a Constellation, then we can do that as is,
+    #  but if it's an Instrument, we just have to put that Instrument
+    #  into something that will yield that Instrument, like a list.
     if isinstance(const, pysat.Instrument):
         const = [const]
     elif not isinstance(const, pysat.Constellation):
         raise ValueError("Parameter must be an Instrument or a Constellation.")
 
-    # create bins
-    # seems to create the boundaries used for sorting into bins
+    # Create the boundaries used for sorting into bins
     if auto_bin:
         binx = np.linspace(bin1[0], bin1[1], bin1[2] + 1)
     else:
         binx = np.array(bin1)
 
-    # how many bins are used
+    # How many bins are used
     numx = len(binx) - 1
-    # how many different data products
+    # How many different data products
     numz = len(data_label)
 
-    # create array to store all values before taking median
-    # the indices of the bins/data products? used for looping.
+    # Create array to store all values before taking median.
+    # The indices of the bins used for looping.
     xarr = np.arange(numx)
     zarr = np.arange(numz)
-    # 3d array:  stores the data that is sorted into each bin? - in a deque
+    # 3d array:  stores the data that is sorted into each bin - in a deque
     ans = [[collections.deque() for i in xarr] for k in zarr]
 
-    for inst in const:
-        # do loop to iterate over instrument season
-        # probably iterates by date but that all depends on the
-        # configuration of that particular instrument.
-        # either way, it iterates over the instrument, loading successive
-        # data between start and end bounds
-        for inst in inst:
-            # collect data in bins for averaging
+    for inst1 in const:
+        # Iterate over instrument season. Probably iterates by date but that
+        # all depends on the configuration of that particular instrument.
+        # Either way, it iterates over the instrument, loading successive
+        # data between start and end bounds.
+        for inst in inst1:
+            # Collect data in bins for averaging
             if len(inst.data) != 0:
-                # sort the data into bins (x) based on label 1
+                # Sort the data into bins (x) based on `label1`
                 # (stores bin indexes in xind)
                 xind = np.digitize(inst.data[label1], binx) - 1
-                # for each possible x index
+                # For each possible x index
                 for xi in xarr:
-                    # get the indicies of those pieces of data in that bin
+                    # Get the indices of those pieces of data in that bin
                     xindex, = np.where(xind == xi)
                     if len(xindex) > 0:
-                        # for each data product label zk
+                        # For each data product label zk
                         for zk in zarr:
-                            # take the data (already filtered by x), select the
-                            # data, put it in a list, and extend the deque
+                            # Take the data (already filtered by x), select the
+                            # data, put it in a list, and extend the deque.
                             idata = inst.data.iloc[xindex]
                             ans[zk][xi].extend(idata[data_label[zk]].tolist())
 
@@ -128,18 +125,16 @@ def median2D(const, bin1, label1, bin2, label2, data_label,
 
     """
 
-    # const is either an Instrument or a Constellation, and we want to
-    #  iterate over it.
-    # If it's a Constellation, then we can do that as is, but if it's
-    #  an Instrument, we just have to put that Instrument into something
-    #  that will yeild that Instrument, like a list.
+    # `const` is either an Instrument or a Constellation, and we want to
+    #  iterate over it. If it's a Constellation, then we can do that as is,
+    #  but if it's an Instrument, we just have to put that Instrument
+    #  into something that will yield that Instrument, like a list.
     if isinstance(const, pysat.Instrument):
         const = [const]
     elif not isinstance(const, pysat.Constellation):
         raise ValueError("Parameter must be an Instrument or a Constellation.")
 
-    # create bins
-    # seems to create the boundaries used for sorting into bins
+    # Create the boundaries used for sorting into bins
     if auto_bin:
         binx = np.linspace(bin1[0], bin1[1], bin1[2] + 1)
         biny = np.linspace(bin2[0], bin2[1], bin2[2] + 1)
@@ -147,58 +142,57 @@ def median2D(const, bin1, label1, bin2, label2, data_label,
         binx = np.array(bin1)
         biny = np.array(bin2)
 
-    # how many bins are used
+    # How many bins are used
     numx = len(binx) - 1
     numy = len(biny) - 1
-    # how many different data products
+    # How many different data products
     numz = len(data_label)
 
-    # create array to store all values before taking median
-    # the indices of the bins/data products? used for looping.
+    # Create array to store all values before taking median
+    # The indices of the bins/data products. Used for looping.
     yarr = np.arange(numy)
     xarr = np.arange(numx)
     zarr = np.arange(numz)
-    # 3d array:  stores the data that is sorted into each bin? - in a deque
+    # 3d array:  stores the data that is sorted into each bin - in a deque
     ans = [[[collections.deque() for i in xarr] for j in yarr] for k in zarr]
 
     for inst1 in const:
-        # do loop to iterate over instrument season
-        # probably iterates by date but that all depends on the
-        # configuration of that particular instrument.
-        # either way, it iterates over the instrument, loading successive
-        # data between start and end bounds
+        # Iterate over instrument season. Probably iterates by date but that
+        # all depends on the configuration of that particular instrument.
+        # Either way, it iterates over the instrument, loading successive
+        # data between start and end bounds.
         for inst in inst1:
-            # collect data in bins for averaging
+            # Collect data in bins for averaging
             if len(inst.data) != 0:
-                # sort the data into bins (x) based on label 1
+                # Sort the data into bins (x) based on label 1
                 # (stores bin indexes in xind)
                 xind = np.digitize(inst.data[label1], binx) - 1
-                # for each possible x index
+                # For each possible x index
                 for xi in xarr:
-                    # get the indicies of those pieces of data in that bin
+                    # Get the indices of those pieces of data in that bin.
                     xindex, = np.where(xind == xi)
                     if len(xindex) > 0:
-                        # look up the data along y (label2) at that set of
-                        # indices (a given x)
+                        # Look up the data along y (label2) at that set of
+                        # indices (a given x).
                         yData = inst.data.iloc[xindex]
                         # digitize that, to sort data into bins along y
                         # (label2) (get bin indexes)
                         yind = np.digitize(yData[label2], biny) - 1
-                        # for each possible y index
+                        # For each possible y index
                         for yj in yarr:
-                            # select data with this y index (and we already
+                            # Select data with this y index (and we already
                             # filtered for this x index)
                             yindex, = np.where(yind == yj)
                             if len(yindex) > 0:
-                                # for each data product label zk
+                                # For each data product label zk
                                 for zk in zarr:
-                                    # take the data (already filtered by x)
-                                    # filter it by y and
-                                    # select the data product, put it in a
-                                    # list, and extend the deque
-                                    indlab = yData.columns.get_loc(data_label[zk])
-                                    ans[zk][yj][xi].extend(yData.iloc[yindex,
-                                                                      indlab].tolist())
+                                    # Take the data (already filtered by x),
+                                    # filter it by y, select the data product,
+                                    # put it in a list, and extend the deque.
+                                    indlab = yData.columns.get_loc(
+                                        data_label[zk])
+                                    ans[zk][yj][xi].extend(
+                                        yData.iloc[yindex, indlab].tolist())
 
     return _calc_2d_median(ans, data_label, binx, biny, xarr, yarr, zarr,
                            numx, numy, numz, returnData)
