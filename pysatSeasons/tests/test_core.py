f1844784463f1d58f03e5f2c1254101ab524c34e
Russell Stoneback <rstoneba@utdallas.edu>
2021-11-19 16:28:33 -0600

    TST/BUG: Fixed use of `inst_module` and added test for list of numbers.

--- pysatSeasons/tests/test_core.py
+++ pysatSeasons/tests/test_core.py
@@ -11,7 +11,7 @@ import pysatSeasons as ps
 class TestCore(object):
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.testInst = pysat.Instrument(inst_mod=pinsts.pysat_testing,
+        self.testInst = pysat.Instrument(inst_module=pinsts.pysat_testing,
                                          clean_level='clean')
         self.bounds1 = self.testInst.inst_module._test_dates['']['']
 
@@ -38,6 +38,16 @@ class TestCore(object):
         assert 'pysat_binning' not in self.out.dims
         return
 
+    def test_comp_form_numbers(self):
+        """Test computational_form with [float1, float2, ...., floatn]"""
+
+        self.testInst.load(date=self.bounds1)
+
+        self.out = ps.computational_form(self.testInst['mlt'].values.tolist())
+        assert isinstance(self.out, xr.Dataset)
+        assert 'pysat_binning' not in self.out.dims
+        return
+
     def test_comp_form_list_vars(self):
         """Test computational_form with [inst[var], inst[var2], ...]"""
         self.testInst.load(date=self.bounds1)
@@ -60,7 +70,7 @@ class TestCore(object):
 class TestCoreXarray(TestCore):
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.testInst = pysat.Instrument(inst_mod=pinsts.pysat_testing_xarray,
+        self.testInst = pysat.Instrument(inst_module=pinsts.pysat_testing_xarray,
                                          clean_level='clean')
         self.bounds1 = self.testInst.inst_module._test_dates['']['']
 
